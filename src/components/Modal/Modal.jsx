c677ba8b66e24e0ab985757eecb9d8e8e6351740
Andrew Butenko <sedkol88@gmail.com>
2024-01-26 02:12:16 +0200

    fixing problem Modal
    исправление ошибки, которая препятствовала деплою

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,22 +1,41 @@
-import { useEffect } from 'react';
+import { useEffect, useCallback } from 'react';
 import { createPortal } from 'react-dom';
 
 import styles from './modal.module.css';
 
 const modalRoot = document.getElementById('modal-root');
 
+// const Modal = ({ close, children }) => {
+//   const closeModal = ({ target, currentTarget, code }) => {
+//     if (target === currentTarget || code === 'Escape') {
+//       close();
+//     }
+//   };
+
+//   useEffect(() => {
+//     document.addEventListener('keydown', closeModal);
+
+//     return () => document.removeEventListener('keydown', closeModal);
+//   }, [close, closeModal]);
+
+// =======================================================
 const Modal = ({ close, children }) => {
-  const closeModal = ({ target, currentTarget, code }) => {
-    if (target === currentTarget || code === 'Escape') {
-      close();
-    }
-  };
+  const closeModal = useCallback(
+    ({ target, currentTarget, code }) => {
+      if (target === currentTarget || code === 'Escape') {
+        close();
+      }
+    },
+    [close]
+  );
 
   useEffect(() => {
-    document.addEventListener('keydown', closeModal);
+    const handleKeyDown = event => closeModal(event);
+    document.addEventListener('keydown', handleKeyDown);
 
-    return () => document.removeEventListener('keydown', closeModal);
-  }, []);
+    return () => document.removeEventListener('keydown', handleKeyDown);
+  }, [closeModal]);
+  // =======================================================
 
   return createPortal(
     <div onClick={closeModal} className={styles.overlay}>
